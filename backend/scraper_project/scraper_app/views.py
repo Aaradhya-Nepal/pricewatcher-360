bf9836c814a0593b5cf79601ff41d9b9fa743daa
Aaradhya-Nepal <aaradhyanepal1@gmail.com>
2023-09-26 16:43:02 +0545

    Create api for front end to receive the data

--- backend/scraper_project/scraper_app/views.py
+++ backend/scraper_project/scraper_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import ScrapedProduct
 
-# Create your views here.
+def get_data(request):
+    data = ScrapedProduct.objects.all()
+    serialized_data = serialize_data(data)
+    return JsonResponse(serialized_data, safe=False)
+
+def serialize_data(data):
+    # Serialize your data into a format like JSON
+    serialized_data = [{'field1': item.field1, 'field2': item.field2} for item in data]
+    return serialized_data
--- backend/scraper_project/scraper_project/urls.py
+++ backend/scraper_project/scraper_project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('api/get_data/', views.get_data, name='get_data'),
 ]
