73ef941450822f04d7ac5e0887ceb6af5c236453
Aaradhya-Nepal <aaradhyanepal1@gmail.com>
2023-09-26 18:50:17 +0545

    Scraped the daraz store after passign the data from react frontend

--- backend/scraper_project/scraper_app/views.py
+++ backend/scraper_project/scraper_app/views.py
@@ -1,6 +1,10 @@
+import subprocess
 from django.http import JsonResponse
 from scraper_app.models import ScrapedProduct
 from django.views.decorators.csrf import csrf_exempt
+import subprocess
+import json
+import requests
 
 def get_data(request):
     data = ScrapedProduct.objects.all()
@@ -34,21 +38,37 @@ def search_view(request):
         if 'search_term' in request.POST:
             # Retrieve the search term from the POST data
             search_term = request.POST['search_term']
-            
-            # Perform the search and prepare the response data
-            # You can use the search_term variable here
-            # For example, you can use it to search your database or scrape data
-            # For now, let's create a sample response data
-            data = {
-                'results': [
-                    {'title': 'Product 1', 'price': 10.99},
-                    {'title': 'Product 2', 'price': 15.99},
-                    # Add more results as needed
-                ]
-            }
-            
-            # Return the response data as JSON
-            return JsonResponse(data)
+
+            # Trigger the Scrapy spider with the search term as a command-line argument
+            spider_name = 'daraz'  # Replace with your actual spider name
+            max_pages = 2  # Replace with your desired max pages
+
+            spider_directory = 'C:\\Users\\Aaradhya\\Documents\\Projects\\pricewatcher-360\\backend\\scraper_project\\scraper\\scraper\\spiders'
+
+            try:
+                # Run the Scrapy spider using subprocess
+                subprocess.run([
+                    'scrapy',
+                    'crawl',
+                    spider_name,
+                    '-a',
+                    f'search_term={search_term}',
+                    '-a',
+                    f'max_pages={max_pages}'
+                ], cwd=spider_directory)
+
+                # You can customize the response based on the result of the spider if needed
+                response_data = {
+                    'message': 'Spider started successfully',
+                    'search_term': search_term
+                }
+
+                # Return the response data as JSON
+                return JsonResponse(response_data)
+            except Exception as e:
+                # Handle any errors that occur when running the spider
+                return JsonResponse({'error': str(e)}, status=500)
+
         else:
             # If 'search_term' is not found in the POST data, return an error response
             return JsonResponse({'error': 'Missing search_term in POST data'})
